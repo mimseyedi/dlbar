da8f71c4ec3f49fcd17de3cfae6f8c1317103a3a
Mohammad <mim.seyedi@gmail.com>
2023-06-07 23:55:40 +0330

    The __init__ file was added along with the relevant specifications.

---
+++ dlbar/__init__.py
@@ -0,0 +1,21 @@
+"""
+██████╗ ██╗     ██████╗  █████╗ ██████╗
+██╔══██╗██║     ██╔══██╗██╔══██╗██╔══██╗
+██║  ██║██║     ██████╔╝███████║██████╔╝
+██║  ██║██║     ██╔══██╗██╔══██║██╔══██╗
+██████╔╝███████╗██████╔╝██║  ██║██║  ██║
+╚═════╝ ╚══════╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝
+
+dlbar is a simple terminal progress bar for downloading and displaying download progress.
+
+This is an open source project under the MIT license. You can easily use it or make any changes you like.
+Also, your contribution to the project through the GitHub page will be welcomed.
+
+Github repository: https://github.com/mimseyedi/dlbar
+"""
+
+from dlbar.dlbar import DownloadBar
+
+__author__ = 'mimseyedi'
+__version__ = '1.0.0'
+__date__ = '2023-06-07'
